187fd57f0b6d05cc3b395932ba72298b648ff947
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-11-04 14:12:47 +0100

    Export WarpaintEditor

--- src/tf2/warpaints/export.ts
+++ src/tf2/warpaints/export.ts
@@ -1,4 +1,5 @@
 export * from './legacypaintkits';
 export { PaintKitDefinitions } from './paintkitdefinitions';
 export { TextureCombiner } from './texturecombiner';
+export { WarpaintEditor } from './warpainteditor';
 export { WeaponManager, WeaponManagerEventTarget } from './weaponmanager';
